430b644737db824de61868163ca1f398bca643bf
Gökhan Bingöl <gbingol@hotmail.com>
2024-01-14 20:06:40 +0100

    changes to cdate class

--- macroland/util/cdate.cpp
+++ macroland/util/cdate.cpp
@@ -11,8 +11,6 @@ namespace util
 		time_t Time = std::time(0);
 		std::tm* TimeInfo = std::localtime(&Time);
 
-		//Since subsequent calls change the time, we need to store them so that time will not change when same CDate object is called
-
 		m_Year = 1900 + TimeInfo->tm_year;
 		m_Month = TimeInfo->tm_mon + 1;
 		m_Day = TimeInfo->tm_mday;
@@ -21,125 +19,66 @@ namespace util
 		m_Second = TimeInfo->tm_sec;
 	}
 
-	CDate::CDate(const time_t& time)
+	std::string CDate::year() const {
+		return std::to_string(m_Year);
+	}
+
+	std::string CDate::month() const
 	{
-		time_t Time = time;
-		std::tm* TimeInfo = std::localtime(&Time);
-
-		//Since subsequent calls change the time, we need to store them so that time will not change when same CDate object is called
-		m_Year = 1900 + TimeInfo->tm_year;
-		m_Month = TimeInfo->tm_mon + 1;
-		m_Day = TimeInfo->tm_mday;
-		m_Hour = TimeInfo->tm_hour;
-		m_Minute = TimeInfo->tm_min;
-		m_Second = TimeInfo->tm_sec;
+		return (m_Month < 10) ?
+			std::string("0") + std::to_string(m_Month) :
+			std::to_string(m_Month);
 	}
 
-	CDate& CDate::operator=(const CDate& other)
+	std::string CDate::day() const
 	{
-		if (this == &other)
-			return *this;
-
-		m_Year = other.m_Year;
-		m_Month = other.m_Month;
-		m_Day = other.m_Day;
-		m_Hour = other.m_Hour;
-		m_Minute = other.m_Minute;
-		m_Second = other.m_Second;
-
-		return *this;
+		return (m_Day < 10) ?
+			std::string("0") + std::to_string(m_Day) :
+			std::to_string(m_Day);
 	}
 
-
-	int CDate::GetYear() const {
-		return m_Year;
-	}
-
-	int CDate::GetMonth() const {
-		return m_Month;
-	}
-
-	int CDate::GetDay() const {
-		return m_Day;
-	}
-
-	int CDate::GetHour() const {
-		return m_Hour;
-	}
-
-	int CDate::GetMinute() const {
-		return m_Minute;
-	}
-
-	int CDate::GetSecond() const {
-		return m_Second;
-	}
-
-	std::string CDate::GetYearStr() const {
-		return std::to_string(GetYear());
-	}
-
-	std::string CDate::GetMonthStr(bool Name) const
+	std::string CDate::hour() const
 	{
-		std::string Months[] = {
-			"January", "February", "March", "April", "May", "June",
-			"July", "August", "September", "October", "November", "December"
-		};
-
-		if (Name) return Months[GetMonth() - 1];
-
-		return (GetMonth() < 10) ?
-			std::string("0") + std::to_string(GetMonth()) :
-			std::to_string(GetMonth());
+		return (m_Hour < 10) ?
+			std::string("0") + std::to_string(m_Hour) :
+			std::to_string(m_Hour);
 	}
 
-	std::string CDate::GetDayStr() const
+	std::string CDate::minute() const
 	{
-		return (GetDay() < 10) ?
-			std::string("0") + std::to_string(GetDay()) :
-			std::to_string(GetDay());
+		return (m_Minute < 10) ?
+			std::string("0") + std::to_string(m_Minute) :
+			std::to_string(m_Minute);
 	}
 
-	std::string CDate::GetHourStr() const
+	std::string CDate::second() const
 	{
-		return (GetHour() < 10) ?
-			std::string("0") + std::to_string(GetHour()) :
-			std::to_string(GetHour());
-	}
-
-	std::string CDate::GetMinuteStr() const
-	{
-		return (GetMinute() < 10) ?
-			std::string("0") + std::to_string(GetMinute()) :
-			std::to_string(GetMinute());
-	}
-
-	std::string CDate::GetSecondStr() const
-	{
-		return (GetSecond() < 10) ?
-			std::string("0") + std::to_string(GetSecond()) :
-			std::to_string(GetSecond());
+		return (m_Second < 10) ?
+			std::string("0") + std::to_string(m_Second) :
+			std::to_string(m_Second);
 	}
 
 
 	bool CDate::operator<(const CDate& other)
 	{
-		return  !(GetYear() > other.GetYear() ||
-			GetMonth() > other.GetMonth() ||
-			GetDay() > other.GetDay() ||
-			GetHour() > other.GetHour() ||
-			GetMinute() > other.GetMinute() ||
-			GetSecond() >= other.GetSecond());
+		return  !(
+			m_Year > other.m_Year ||
+			m_Month > other.m_Month ||
+			m_Day > other.m_Day ||
+			m_Hour > other.m_Hour ||
+			m_Minute > other.m_Minute ||
+			m_Second >= other.m_Second);
 	}
 
 	bool CDate::operator==(const CDate& other)
 	{
-		return (GetYear() == other.GetYear() &&
-			GetMonth() == other.GetMonth() &&
-			GetDay() == other.GetDay() &&
-			GetHour() == other.GetHour() &&
-			GetMinute() == other.GetMinute() &&
-			GetSecond() == other.GetSecond());
+		return (
+			m_Year == other.m_Year &&
+			m_Month == other.m_Month &&
+			m_Day == other.m_Day &&
+			m_Hour == other.m_Hour &&
+			m_Minute == other.m_Minute &&
+			m_Second == other.m_Second);
 	}
 
 	bool CDate::operator>(const CDate& other)
--- macroland/util/cdate.h
+++ macroland/util/cdate.h
@@ -3,73 +3,55 @@
 #include <string>
 #include <time.h>
 
+
 //TODO: Use C++20 date and time utilities, no need for CDate
 namespace util
 {
 	class CDate
 	{
-
 	public:
 		CDate();
-		CDate(const time_t& time);
 
 		CDate(const CDate& another) = default;
-		CDate& operator=(const CDate& other);
+		CDate& operator=(const CDate& other) = default;
+
+		bool operator<(const CDate& other);
+		bool operator==(const CDate& other);
+		bool operator>(const CDate& other);
 
 		~CDate() = default;
 
-
-		int GetYear() const;
-
-		//returns a value between 1 and 12
-		int GetMonth() const;
-
-		//returns a value between 1-31
-		int GetDay() const;
-
-		//returns a value between 0-23
-		int GetHour() const;
-
-		//returns a value between 0-59
-		int GetMinute() const;
-
-		//returns a value between 0-59
-		int GetSecond() const;
-
-		std::string GetYearStr() const;
+		std::string year() const;
 
 		//if Name=true then returns January, February..., otherwise appends 0 if month is smaller than 10
-		std::string GetMonthStr(bool Name = false) const;
+		std::string month() const;
 
 		//if day is smaller than 10, appends zero
-		std::string GetDayStr() const;
+		std::string day() const;
 
 		//if hour is smaller than 10, appends zero
-		std::string GetHourStr() const;
+		std::string hour() const;
 
 		//if minute is smaller than 10, appends zero
-		std::string GetMinuteStr() const;
+		std::string minute() const;
 
 		//if second is smaller than 10, appends zero
-		std::string GetSecondStr() const;
+		std::string second() const;
 
 		//Get the current time as hour:minute:second
-		std::string GetTime(const char* sep = ":") const {
-			return GetHourStr() + sep + GetMinuteStr() + sep + GetSecondStr();
+		std::string GetTime(const char* sep = ":") const 
+		{
+			return hour() + sep + minute() + sep + second();
 		}
 
 		//Gets the date as: day/month/year, reversed=true -> year/month/day 
 		std::string GetDate(const char* sep = "/", bool reversed = false) const
 		{
 			if (reversed)
-				return GetYearStr() + sep + GetMonthStr() + sep + GetDayStr();
+				return year() + sep + month() + sep + day();
 
-			return  GetDayStr() + sep + GetMonthStr() + sep + GetYearStr();
+			return  day() + sep + month() + sep + year();
 		}
-
-		bool operator<(const CDate& other);
-		bool operator==(const CDate& other);
-		bool operator>(const CDate& other);
 
 	private:
 		int m_Year, m_Month, m_Day;
