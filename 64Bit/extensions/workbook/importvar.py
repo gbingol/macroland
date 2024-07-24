29e738497cbf6525623b32eb962d0025a1188b12
Gökhan Bingöl <gbingol@hotmail.com>
2024-07-24 15:53:09 +0200

    changed dots to tabs

--- 64Bit/extensions/workbook/importvar.py
+++ 64Bit/extensions/workbook/importvar.py
@@ -6,35 +6,35 @@ from _sci import (Worksheet, Range, activeworksheet,
 
 
 class GotoLabel(Exception): 
-    """Serves as a label"""
-    pass
+	"""Serves as a label"""
+	pass
 
 if __name__ == '__main__':
-    try:
-        ws = activeworksheet()
+	try:
+		ws = activeworksheet()
 
-        msg = """
-        Enter a valid expression that returns list | dict | str | int | float.
+		msg = """
+		Enter a valid expression that returns list | dict | str | int | float.
 
-        Any variables used in the expression must already be defined in the 
-        command window.
-        """
-        expression = GetTextFromUser(msg, "Enter an expression")
-        if expression == "":
-            raise GotoLabel("")
-        
-        result = eval(expression, CommandWindowDict)
-        assert isinstance(result, list|dict|str|int|float), "expected list|dict|str|int|float"
+		Any variables used in the expression must already be defined in the 
+		command window.
+		"""
+		expression = GetTextFromUser(msg, "Enter an expression")
+		if expression == "":
+			raise GotoLabel("")
+		
+		result = eval(expression, CommandWindowDict)
+		assert isinstance(result, list|dict|str|int|float), "expected list|dict|str|int|float"
 
-        rng = ws.selection()
-        ncols, nrows = 0, 0
-        if rng!=None:
-            ncols, nrows = rng.ncols(), rng.nrows()
-        
-        CurRow, CurCol = ws.cursor()
-        ws.writelist(result, CurRow, CurCol)
-        
-    except GotoLabel:
-        pass
-    except Exception as e:
-        messagebox(str(e), "Import Error!")
+		rng = ws.selection()
+		ncols, nrows = 0, 0
+		if rng!=None:
+			ncols, nrows = rng.ncols(), rng.nrows()
+		
+		CurRow, CurCol = ws.cursor()
+		ws.writelist(result, CurRow, CurCol)
+		
+	except GotoLabel:
+		pass
+	except Exception as e:
+		messagebox(str(e), "Import Error!")
