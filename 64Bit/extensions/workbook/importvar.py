512bf82d873f4c48284c9cb855d099dc33519c57
Gökhan Bingöl <gbingol@hotmail.com>
2024-07-28 14:33:51 +0200

    work on dlgExpressionEval

--- 64Bit/extensions/workbook/importvar.py
+++ 64Bit/extensions/workbook/importvar.py
@@ -1,4 +1,4 @@
-from wx import GetTextFromUser 
+import wx
 from _sci import (Workbook, CommandWindowDict, messagebox)
 import numpy as _np
 
@@ -8,6 +8,70 @@ class GotoLabel(Exception):
 	"""Serves as a label"""
 	pass
 
+
+class dlgExpressionEval ( wx.Dialog ):
+
+	def __init__( self, parent ):
+		super().__init__ ( parent, title = "Enter expressions", style = wx.STAY_ON_TOP )
+		
+		msg = """
+Enter a valid expression that returns ndarray|list|dict|str|int|float.
+
+IMPORTANT: Any variables used in the expression must already be defined in the command window.
+
+TIP: For 1 dimensional containers, selecting more rows than columns will write the result 
+of the expression as row-wise, otherwise will be written as column-wise.
+"""
+		self.m_stTxt = wx.StaticText( self, label=msg )
+
+		self.m_scintilla = wx.stc.StyledTextCtrl( self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, 0)
+		self.m_scintilla.SetUseTabs ( True )
+		self.m_scintilla.SetTabWidth ( 4 )
+		self.m_scintilla.SetIndent ( 4 )
+		self.m_scintilla.SetTabIndents( True )
+		self.m_scintilla.SetBackSpaceUnIndents( True )
+		self.m_scintilla.SetViewEOL( False )
+		self.m_scintilla.SetViewWhiteSpace( True )
+		self.m_scintilla.SetMarginWidth( 2, 0 )
+		self.m_scintilla.SetIndentationGuides( True )
+		self.m_scintilla.SetReadOnly( False )
+		self.m_scintilla.SetMarginWidth( 1, 0 )
+		self.m_scintilla.SetMarginType( 0, wx.stc.STC_MARGIN_NUMBER )
+		self.m_scintilla.SetMarginWidth( 0, self.m_scintilla.TextWidth( wx.stc.STC_STYLE_LINENUMBER, "_99999" ) )
+		self.m_scintilla.MarkerDefine( wx.stc.STC_MARKNUM_FOLDER, wx.stc.STC_MARK_BOXPLUS )
+		self.m_scintilla.MarkerSetBackground( wx.stc.STC_MARKNUM_FOLDER, wx.BLACK)
+		self.m_scintilla.MarkerSetForeground( wx.stc.STC_MARKNUM_FOLDER, wx.WHITE)
+		self.m_scintilla.MarkerDefine( wx.stc.STC_MARKNUM_FOLDEROPEN, wx.stc.STC_MARK_BOXMINUS )
+		self.m_scintilla.MarkerSetBackground( wx.stc.STC_MARKNUM_FOLDEROPEN, wx.BLACK )
+		self.m_scintilla.MarkerSetForeground( wx.stc.STC_MARKNUM_FOLDEROPEN, wx.WHITE )
+		self.m_scintilla.MarkerDefine( wx.stc.STC_MARKNUM_FOLDERSUB, wx.stc.STC_MARK_EMPTY )
+		self.m_scintilla.MarkerDefine( wx.stc.STC_MARKNUM_FOLDEREND, wx.stc.STC_MARK_BOXPLUS )
+		self.m_scintilla.MarkerSetBackground( wx.stc.STC_MARKNUM_FOLDEREND, wx.BLACK )
+		self.m_scintilla.MarkerSetForeground( wx.stc.STC_MARKNUM_FOLDEREND, wx.WHITE )
+		self.m_scintilla.MarkerDefine( wx.stc.STC_MARKNUM_FOLDEROPENMID, wx.stc.STC_MARK_BOXMINUS )
+		self.m_scintilla.MarkerSetBackground( wx.stc.STC_MARKNUM_FOLDEROPENMID, wx.BLACK)
+		self.m_scintilla.MarkerSetForeground( wx.stc.STC_MARKNUM_FOLDEROPENMID, wx.WHITE)
+		self.m_scintilla.MarkerDefine( wx.stc.STC_MARKNUM_FOLDERMIDTAIL, wx.stc.STC_MARK_EMPTY )
+		self.m_scintilla.MarkerDefine( wx.stc.STC_MARKNUM_FOLDERTAIL, wx.stc.STC_MARK_EMPTY )
+		self.m_scintilla.SetSelBackground( True, wx.SystemSettings.GetColour(wx.SYS_COLOUR_HIGHLIGHT ) )
+		self.m_scintilla.SetSelForeground( True, wx.SystemSettings.GetColour(wx.SYS_COLOUR_HIGHLIGHTTEXT ) )
+
+		sdbSizer = wx.StdDialogButtonSizer()
+		self.m_btnOK = wx.Button( self, wx.ID_OK )
+		sdbSizer.AddButton( self.m_btnOK )
+		self.m_btnCancel = wx.Button( self, wx.ID_CANCEL )
+		sdbSizer.AddButton( self.m_btnCancel )
+		sdbSizer.Realize()
+
+		szrMain = wx.BoxSizer( wx.VERTICAL )
+		szrMain.Add( self.m_stTxt, 0, wx.ALL|wx.EXPAND, 5 )
+		szrMain.Add( self.m_scintilla, 1, wx.EXPAND |wx.ALL, 5 )
+		szrMain.Add( sdbSizer, 0, wx.EXPAND|wx.ALIGN_CENTER_HORIZONTAL, 5 )
+
+		self.SetSizer( szrMain )
+		self.Layout()
+
+		self.Centre( wx.BOTH )
 
 
 
@@ -23,7 +87,7 @@ IMPORTANT: Any variables used in the expression must already be defined in the c
 TIP: For 1 dimensional containers, selecting more rows than columns will write the result 
 of the expression as row-wise, otherwise will be written as column-wise.
 """
-		expression = GetTextFromUser(msg, "Enter an expression")
+		expression = wx.GetTextFromUser(msg, "Enter an expression")
 		if expression == "":
 			raise GotoLabel("")
 		
