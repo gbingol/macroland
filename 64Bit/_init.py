30f6d6450b427650bc327d4f774d64eb49da6828
Gökhan Bingöl <gbingol@hotmail.com>
2024-07-29 13:39:27 +0200

    now scisuit and wxpython are installed in one go if user chooses yes

--- 64Bit/_init.py
+++ 64Bit/_init.py
@@ -1,11 +1,3 @@
-"""
-It is recommended NOT to change or to edit this file
-
-This file is run when the system starts, so anything wrong here will 
-either prevent system from starting or will break things
-"""
-
-
 
 """
 When designing wxPython apps do not use app.MainLoop in your application
@@ -21,23 +13,23 @@ import sys as _sys
 
 from __SCISUIT import GUI as _gui # type: ignore
 
-def assert_pkg(name:str, pip:str)->bool:
+
+def installcrucialpkg(name:str):
 	
-	#package already installed
 	x = pkgutil.iter_modules()
 	for i in x:
 		if i.ispkg==True and i.name == name:
-			return True
+			return
 	
 	PyHome = _sys.exec_prefix
 	PyExe = PyHome + os.sep + "python.exe"
 	Cmd = "\"" + PyExe + "\""
-	Cmd += " -m pip install " + pip
+	Cmd += " -m pip install scisuit wxPython" #install both in one go
 
 	Msg = name + " is missing which is crucial for MacroLand App. Wanna install? \n \n"
 	Msg += "Choosing Yes will launch the terminal and installation process using the following command: \n \n"	
 	Msg += Cmd + "\n \n"
-	Msg += "If you choose No, you might have to manually install the package to run the requiring app."
+	Msg += "If you choose No, you might have to manually install to the above-shown path."
 
 	YesNo = _gui.messagebox(Msg, "Install " + name + "?", yesno=True)
 	if not YesNo:
@@ -46,9 +38,8 @@ def assert_pkg(name:str, pip:str)->bool:
 	os.system(Cmd)
 
 
-
-assert_pkg("scisuit", "scisuit")
-assert_pkg("wx", "wxPython")
+installcrucialpkg("scisuit")
+installcrucialpkg("wx")
 
 import wx
 app=wx.App(useBestVisual = True)
