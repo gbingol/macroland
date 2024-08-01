7f6f8f44a727a91882631b2904dca689a8342085
Gökhan Bingöl <gbingol@hotmail.com>
2024-08-01 16:35:03 +0200

    added prettify

--- 64Bit/_sci/__init__.py
+++ 64Bit/_sci/__init__.py
@@ -7,6 +7,6 @@ CommandWindowDict:dict = __cmd.__dict__
 
 
 from .widgets import Frame, GridTextCtrl, pnlOutputOptions, NumTextCtrl, ScintillaCtrl
-from .util import parent_path, colnum2label, assert_pkg
+from .util import parent_path, colnum2label, assert_pkg, prettify
 from .icell import Workbook, Worksheet, Range
 from .framework import Framework
