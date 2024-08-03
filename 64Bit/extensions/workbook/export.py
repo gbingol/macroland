c171f9222aa341da823229cfbae9ddd578a8594f
gbingol <gbingol@hotmail.com>
2024-08-03 22:03:49 +0200

    improvements to export and frmimport (data export and import)

--- 64Bit/extensions/workbook/export.py
+++ 64Bit/extensions/workbook/export.py
@@ -6,23 +6,29 @@ from _sci import Workbook, Framework
 
 
 def Save(param):
-	Framework.Enable(False)
-	
-	wildcard = "Text files (*.txt)|*.txt"  if param =="txt" else "CSV files (*.csv)|*.csv"
-	dlg = wx.FileDialog(None, style=wx.FD_SAVE, wildcard= wildcard)
-	dlg.ShowModal()
+	try:
+		ws = Workbook().activeworksheet()
+		rng = ws.selection()
+		if rng.ncols()<2:
+			raise RuntimeError("At least 2 columns of data required")
+		
+		MainLst = rng.tolist(axis = 1)
 
-	Framework.Enable(True)
-	
-	path = str(dlg.GetPath())
-	if path =="":
-		return
-	
-	ws = Workbook().activeworksheet()
-	rng = ws.selection()
-	MainLst = rng.tolist(axis = 1)
-	
-	arr = np.array(MainLst)
-	arr[arr == None] = ""
-	
-	np.savetxt(path, arr, delimiter="\t" if param == "txt" else ",", fmt="%s")
+		Framework.Enable(False)
+		
+		wildcard = "Text files (*.txt)|*.txt"  if param =="txt" else "CSV files (*.csv)|*.csv"
+		dlg = wx.FileDialog(None, style=wx.FD_SAVE, wildcard= wildcard)
+		dlg.ShowModal()
+
+		Framework.Enable(True)
+		
+		path = str(dlg.GetPath())
+		if path =="":
+			return
+		
+		arr = np.array(MainLst)
+		arr[arr == None] = ""
+		
+		np.savetxt(path, arr, delimiter="\t" if param == "txt" else ",", fmt="%s")
+	except Exception as e:
+		Framework().messagebox(str(e),  "Export Error")
--- 64Bit/extensions/workbook/frmImport.py
+++ 64Bit/extensions/workbook/frmImport.py
@@ -3,7 +3,7 @@ import pathlib
 import numpy as np
 
 
-from _sci import pnlOutputOptions, Frame, parent_path, messagebox
+from _sci import pnlOutputOptions, Frame, parent_path, Framework
 
 
 
@@ -18,34 +18,35 @@ class frmImportLoc ( Frame ):
 		IconPath = parent_path(__file__) / "icons" / "import.png"
 		self.SetIcon(wx.Icon(str(IconPath)))
 
-		self.SetSizeHints( wx.DefaultSize, wx.DefaultSize )
-
 		self.m_pnlOutput = pnlOutputOptions( self)	
 
-		m_sdbSizer = wx.StdDialogButtonSizer()
-		self.m_sdbSizerOK = wx.Button( self, wx.ID_OK, label = "Import" )
-		m_sdbSizer.AddButton( self.m_sdbSizerOK )
-		self.m_sdbSizerCancel = wx.Button( self, wx.ID_CANCEL, label="Close" )
-		m_sdbSizer.AddButton( self.m_sdbSizerCancel )
-		m_sdbSizer.Realize()
+		btnOK = wx.Button( self, wx.ID_OK, label="Import" )
+		btnCancel = wx.Button( self, wx.ID_CANCEL, label="Close" )
+		sdbSzr = wx.StdDialogButtonSizer()
+		sdbSzr.AddButton( btnOK )
+		sdbSzr.AddButton( btnCancel )
+		sdbSzr.Realize()
 
 		mainSizer = wx.BoxSizer( wx.VERTICAL )
 		mainSizer.Add( self.m_pnlOutput, 0, wx.EXPAND |wx.ALL, 5 )
-		mainSizer.Add( m_sdbSizer, 0, wx.EXPAND, 5 )
+		mainSizer.Add( sdbSzr, 0, wx.EXPAND, 5 )
 		self.SetSizerAndFit( mainSizer )
 		self.Layout()
 
 		self.Centre( wx.BOTH )
 
-		self.m_sdbSizerCancel.Bind( wx.EVT_BUTTON, self.__OnCancel )
-		self.m_sdbSizerOK.Bind( wx.EVT_BUTTON, self.__OnOK )
+		btnCancel.Bind( wx.EVT_BUTTON, self.__OnCancel )
+		btnOK.Bind( wx.EVT_BUTTON, self.__OnOK )
+
+
 
 	def __OnCancel( self, event ):
 		self.Close()
 		event.Skip()
 	
 
-	def __OnOK( self, event ):
+
+	def __OnOK( self, event:wx.CommandEvent ):
 		try:
 			WS, row, col = self.m_pnlOutput.Get()
 			assert WS != None, "Ouput Options: Selected range is invalid."
@@ -67,22 +68,23 @@ class frmImportLoc ( Frame ):
 			self.Close()
 
 		except Exception as e:
-			messagebox(str(e), "Error")
+			Framework().messagebox(str(e), "Error")
 
 
 
-def GetPath():
+def GetPath()->str:
 	wcard = "CSV files (*.csv)|*.csv"
 	wcard += "|"
 	wcard += "Tab sep text files (*.txt)|*.txt"
 
-	#TODO: This will cause crash if user starts another selection
+	Framework.Enable(False)
+	
 	dlg = wx.FileDialog(None, style= wx.STAY_ON_TOP, wildcard = wcard)
 	dlg.ShowModal()
-	path = str(dlg.GetPath())
 
+	Framework.Enable(True)
 
-	return path
+	return str(dlg.GetPath())
 
 
 
@@ -94,4 +96,4 @@ if __name__ == "__main__":
 			frm.Show()
 
 	except Exception as e:
-		messagebox(str(e))
+		Framework().messagebox(str(e))
