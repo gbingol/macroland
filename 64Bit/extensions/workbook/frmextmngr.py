7c3f4b2cde80d838b4c0c9a70390efc9ca913228
Gökhan Bingöl <gbingol@hotmail.com>
2024-08-06 14:24:55 +0200

    converted all manifest.lua to manifest.json and extension manager written in wxPython loading

--- 64Bit/extensions/workbook/frmextmngr.py
+++ 64Bit/extensions/workbook/frmextmngr.py
@@ -1,20 +1,27 @@
+import json
+import os
+import sys
+from pathlib import Path
+from dataclasses import dataclass
+
 import wx
 import wx.html
 
-import scisuit.stats as stat
 
-from _sci import (Frame, GridTextCtrl, NumTextCtrl, pnlOutputOptions,
-				  Workbook, Range, 
-				  parent_path, prettify)
+@dataclass
+class Extension:
+	data:str #json data in the form of dictionary
+	path:Path
 
 
-
-class frmextensionmngr ( Frame ):
+class frmextensionmngr ( wx.Frame ):
 
 	def __init__( self, parent ):
-		super().__init__ (parent, title="Extension Manager")
+		super().__init__ (parent, 
+			title="Extension Manager",
+			style=wx.CAPTION|wx.CLOSE_BOX|wx.MINIMIZE_BOX|wx.RESIZE_BORDER|wx.TAB_TRAVERSAL)
 
-		IconPath = parent_path(__file__) / "icons" / "extensions.png"
+		IconPath = Path(__file__).parent / "icons" / "extensions.png"
 		self.SetIcon(wx.Icon(str(IconPath)))
 		
 		self.m_split = wx.SplitterWindow(self, style= wx.SP_3D|wx.SP_LIVE_UPDATE)
@@ -43,14 +50,69 @@ class frmextensionmngr ( Frame ):
 
 		self.Centre( wx.BOTH )
 
+		self._LoadExtensions()
 
+		self.Bind(wx.EVT_CLOSE, self.__OnClose)
+		self.m_LWExt.Bind(wx.EVT_LIST_ITEM_SELECTED, self.__OnListEntrySelected)
+	
+
+
+
+	def __OnClose(self, event:wx.CloseEvent):
+		self.Hide()
+		self.Destroy()
+		event.Skip()
 
 	
+	def __OnListEntrySelected(self, event:wx.ListEvent):
+		item = self.m_LWExt.GetNextItem(-1, wx.LIST_NEXT_ALL, wx.LIST_STATE_SELECTED)
+
+		if item<0:
+			event.Skip()
+			return
+		
+		ReadMePath = self._Extensions[item].path / self._Extensions[item].data["readme"]
+
+		if Path.exists(ReadMePath):
+			self.m_HTMLWin.LoadFile(str(ReadMePath))
+
+
+	def _LoadExtensions(self):
+		PyPath =  Path(sys.exec_prefix)
+		ExtensionPath = PyPath.parent / "extensions"
+
+		self._Extensions:list[Extension] = []
+		for d in os.scandir(str(ExtensionPath)):
+			if d.is_dir():
+				manifest = Path(d) / "manifest.json"
+				f = open(manifest)
+				self._Extensions.append(Extension(data=json.load(f), path=manifest.parent))
+		
+		for index, extension in enumerate(self._Extensions):
+			self.m_LWExt.InsertItem(index, extension.data["extname"])
+			self.m_LWExt.SetItem(index, 1, extension.data["version"])
+			self.m_LWExt.SetItem(index, 2, extension.data["developer"])
+			self.m_LWExt.SetItem(index, 3, extension.data["short_desc"])
+
+
+
 
 if __name__ == "__main__":
 	try:
 		frm = frmextensionmngr(None)
 		frm.Maximize()
 		frm.Show()
+
+		Widths = [ 2,1,2,5 ]
+		Sum = sum(Widths)
+
+		frmWidth = frm.GetClientSize()[0]
+		
+		for i, w in enumerate(Widths):
+			frm.m_LWExt.SetColumnWidth(i, int((frmWidth*w)/Sum))
+		
+		frm.Layout()
+		frm.Refresh()
+
 	except Exception as e:
 		wx.MessageBox(str(e), "Error")
