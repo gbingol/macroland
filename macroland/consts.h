582b2e1536f59322e5a8b2e58750a63222d174a3
Gökhan Bingöl <gbingol@hotmail.com>
2024-08-08 15:00:41 +0200

    copyright and version changed

--- macroland/consts.h
+++ macroland/consts.h
@@ -8,10 +8,7 @@ namespace consts
 	//file that holds the list of reent projects
 	static const char* RECENTPROJ = "recentproj.txt";
 
-	static const char* VERSION = "MacroLand v3.0";
-
-	//Version of scisuit pkg with which the app has been tested
-	static const char *SCISUITPKG = "1.3.5";
+	static const char* VERSION = "MacroLand v3.1 (2024-Aug-08)";
 
 	//name of the temporary directory under MacroLand installation directory
 	static const char* TEMPDIR = "temp";
@@ -22,5 +19,5 @@ namespace consts
 	//configuration directory
 	static const char* EVENTS = "events";
 
-	static const char* COPYRIGHT = "Copyright 2024, PE Bytes (Process Engineering Bytes)";
+	static const char* COPYRIGHT = "Copyright 2024, PE Bytes";
 }
