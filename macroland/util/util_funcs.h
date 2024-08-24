775a873fcd43563fe9a38b98e7b432e2fb4ab59d
Gökhan Bingöl <gbingol@hotmail.com>
2024-08-24 10:43:30 +0200

    removed unused funcs from util

--- macroland/util/util_funcs.h
+++ macroland/util/util_funcs.h
@@ -45,33 +45,10 @@ namespace util
 		std::string_view delim);
 
 
-	/******************************************************** */
 
 
-	void ShowInExplorer(const std::filesystem::path& FullPath);
+	/**************************************************** */
 
-	bool ClipbrdSupportsText();
-
-	wxBitmap CreateBitmap(
-		const wxString& Text,
-		const wxSize& BitmapSize,
-		const wxFont& Font,
-		const wxColour& BG,
-		const wxColour& TxtFG,
-		wxWindow* wnd = nullptr);
-
-	//Get the area as image specified by TopLeft and size
-	wxBitmap GetAsBitmap(
-		wxWindow* window,
-		const wxPoint& TopLeft,
-		const wxSize& Size,
-		wxColor BGColor = wxColor(255, 255, 255));
-
-
-	//Get the client area (whatever drawn there) of a window as bitmap
-	wxBitmap GetAsBitmap(
-		wxWindow* window,
-		wxColor BGColor = wxColor(255, 255, 255));
 
 	wxArrayString GetFontFaces(bool Sorted = true);
 
@@ -104,91 +81,4 @@ namespace util
 		std::filesystem::path m_BaseDir;
 	};
 
-
-	/************************************************************************ */
-
-	class CDate
-	{
-	public:
-		CDate();
-
-		CDate(const CDate& another) = default;
-		CDate& operator=(const CDate& other) = default;
-
-		bool operator<(const CDate& other) const;
-		bool operator==(const CDate& other) const;
-		bool operator>(const CDate& other) const;
-
-		~CDate() = default;
-
-		std::string year() const;
-
-		//if Name=true then returns January, February..., otherwise appends 0 if month is smaller than 10
-		std::string month() const;
-
-		//if day is smaller than 10, appends zero
-		std::string day() const;
-
-		//if hour is smaller than 10, appends zero
-		std::string hour() const;
-
-		//if minute is smaller than 10, appends zero
-		std::string minute() const;
-
-		//if second is smaller than 10, appends zero
-		std::string second() const;
-
-		//Get the current time as hour:minute:second
-		std::string GetTime(const char* sep = ":") const 
-		{
-			return hour() + sep + minute() + sep + second();
-		}
-
-		//Gets the date as: day/month/year, reversed=true -> year/month/day 
-		std::string GetDate(const char* sep = "/", bool reversed = false) const
-		{
-			if (reversed)
-				return year() + sep + month() + sep + day();
-
-			return  day() + sep + month() + sep + year();
-		}
-
-	private:
-		int m_Year, m_Month, m_Day;
-		int m_Hour, m_Minute, m_Second;
-	};
-
-
-
-
-	/******************************************************************** */
-
-	class CRecentFiles
-	{
-	public:
-		//filepath is the file's path which contains/should contain entries
-		CRecentFiles(const std::filesystem::path& filePath);
-
-		//if file exists reads the contents otherwise creates the file
-		bool ReadOrCreate();
-
-		//if file exists, deletes and creates a new one and writes the contents and closes file
-		bool Write();
-
-		void Append(const std::filesystem::path& path);
-
-		auto GetList() const
-		{
-			return m_List;
-		}
-
-		void ClearList()
-		{
-			m_List.clear();
-		}
-
-	private:
-		std::filesystem::path m_FilePath;
-		std::list<std::filesystem::path> m_List;
-	};
 }
