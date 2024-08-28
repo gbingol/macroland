2adf1bff3484836fd8ba85f590f60193861b3746
gbingol <gbingol@hotmail.com>
2024-08-28 11:09:42 +0200

    column-clustered pass all the tests

--- 64Bit/extensions/01 charts/column.py
+++ 64Bit/extensions/01 charts/column.py
@@ -5,9 +5,7 @@ from _sci import Workbook, Framework
 
 
 if __name__ == "__main__":
-	WIDTH = 0.25 #width of each bar
-	GAP = 0.0 # gap between each category bar at a certain level
-
+	
 	try:
 		ws = Workbook().activeworksheet()
 		rng = ws.selection()
@@ -17,43 +15,39 @@ if __name__ == "__main__":
 		SelData = rng.tolist(axis=0)		
 		assert len(SelData)>0, "At least 1 column of data is expected."
 
-		#Single category, only levels selected
-		if(len(SelData) == 1):
-			height = [i for i in SelData[0] if isinstance(i, int|float)]
-			ticks = np.arange(len(height))
-			plt.bar(x=ticks, height=height, width=WIDTH)
-			plt.set_xticks(ticks + WIDTH/2, ticks)
 		
-		else:
-			#If the top cell is None, then that column is the labels column for levels 
-			HasLevLbls = SelData[0][0] == None
-			LevLbls = [i for i in SelData[0] if isinstance(i, str)]
-			
-			HasLevLbls = HasLevLbls or len(LevLbls) == rng.nrows()
-							
-			CategRng = rng.subrange(row=0, col=1) if HasLevLbls else rng
-			CategData = CategRng.tolist(axis=1)
+		#If the top cell is None, then that column is the labels column for levels 
+		HasLevLbls = SelData[0][0] == None
+		LevLbls = [i for i in SelData[0] if isinstance(i, str)]
+		
+		HasLevLbls = HasLevLbls or len(LevLbls) == rng.nrows()
+						
+		CategRng = rng.subrange(row=0, col=1) if HasLevLbls else rng
+		CategData = CategRng.tolist(axis=1)
 
-			CatLbls = [i for i in CategData[0] if isinstance(i, str)]
-			HasCategLabels = len(CatLbls) == CategRng.ncols()
+		CatLbls = [i for i in CategData[0] if isinstance(i, str)]
+		HasCategLabels = len(CatLbls) == CategRng.ncols()
 
-			DataRng = CategRng.subrange(row=1, col=0) if HasCategLabels else CategRng
-			Data = DataRng.tolist(axis=0)
+		DataRng = CategRng.subrange(row=1, col=0) if HasCategLabels else CategRng
+		Data = DataRng.tolist(axis=0)
 
-			Levels = np.arange(DataRng.nrows()) 
+		Levels = np.arange(DataRng.nrows()) 
 
-			mult = 0
-			for i, hgt in enumerate(Data):
-				offset = (WIDTH + GAP)*mult
-				label = CatLbls[i] if HasCategLabels else f"Col {i+1}"
-				plt.bar(x=Levels + offset, height=hgt, width=WIDTH, label=label)
-				mult += 1
+		WIDTH = 0.25 #width of each bar
+		GAP = 0.0 # gap between each category bar at a certain level
+		mult = 0
+		for i, hgt in enumerate(Data):
+			offset = (WIDTH + GAP)*mult
+			label = CatLbls[i] if HasCategLabels else f"Col {i+1}"
+			plt.bar(x=Levels + offset, height=hgt, width=WIDTH, label=label)
+			mult += 1
 
-			plt.set_xticks(Levels+WIDTH, LevLbls if HasLevLbls else Levels+1)
+		plt.set_xticks(Levels+(WIDTH if DataRng.ncols()>1 else WIDTH/2),
+						 LevLbls if HasLevLbls else Levels+1)
 
 			
 		plt.show()
 				
 				
 	except Exception as e:
-		Framework().messagebox(str(e), "Column Error!")
+		Framework().messagebox(str(e), "Column-Clustered Error!")
