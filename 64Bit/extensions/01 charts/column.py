b50fcce671cfbdb2fd273a9c476b1186b717c13f
gbingol <gbingol@hotmail.com>
2024-08-28 10:49:59 +0200

    almost done with colum (clustered)

--- 64Bit/extensions/01 charts/column.py
+++ 64Bit/extensions/01 charts/column.py
@@ -5,6 +5,9 @@ from _sci import Workbook, Framework
 
 
 if __name__ == "__main__":
+	WIDTH = 0.25 #width of each bar
+	GAP = 0.0 # gap between each category bar at a certain level
+
 	try:
 		ws = Workbook().activeworksheet()
 		rng = ws.selection()
@@ -18,38 +21,39 @@ if __name__ == "__main__":
 		if(len(SelData) == 1):
 			height = [i for i in SelData[0] if isinstance(i, int|float)]
 			ticks = np.arange(len(height))
-			plt.bar(x=ticks, height=height)
-			plt.set_xticks(ticks + 0.4, ticks)
+			plt.bar(x=ticks, height=height, width=WIDTH)
+			plt.set_xticks(ticks + WIDTH/2, ticks)
 		
 		else:
-			Levellabels = [i for i in SelData[0] if isinstance(i, str)]
-			LevelsLabeled = len(Levellabels) > 0
+			#If the top cell is None, then that column is the labels column for levels 
+			HasLevLbls = SelData[0][0] == None
+			LevLbls = [i for i in SelData[0] if isinstance(i, str)]
 			
-			RawRng = rng.subrange(row=0, col=1) if LevelsLabeled else rng
-			RawData = RawRng.tolist(axis=1)
+			HasLevLbls = HasLevLbls or len(LevLbls) == rng.nrows()
+							
+			CategRng = rng.subrange(row=0, col=1) if HasLevLbls else rng
+			CategData = CategRng.tolist(axis=1)
 
-			CatLbls = [i for i in RawData[0] if isinstance(i, str)]
-			CatLabeled = len(CatLbls)>0
+			CatLbls = [i for i in CategData[0] if isinstance(i, str)]
+			HasCategLabels = len(CatLbls) == CategRng.ncols()
 
-			DataRng = RawRng.subrange(row=1, col=0) if CatLabeled else RawRng
+			DataRng = CategRng.subrange(row=1, col=0) if HasCategLabels else CategRng
 			Data = DataRng.tolist(axis=0)
 
 			Levels = np.arange(DataRng.nrows()) 
 
-			width = 0.25 
 			mult = 0
-
 			for i, hgt in enumerate(Data):
-				offset = width * mult
-				label = CatLbls[i] if CatLabeled else f"Col {i+1}"
-				plt.bar(x=Levels + offset, height=hgt, width=width, label=label)
+				offset = (WIDTH + GAP)*mult
+				label = CatLbls[i] if HasCategLabels else f"Col {i+1}"
+				plt.bar(x=Levels + offset, height=hgt, width=WIDTH, label=label)
 				mult += 1
 
-			plt.set_xticks(Levels+width, Levellabels if LevelsLabeled else Levels+1)
+			plt.set_xticks(Levels+WIDTH, LevLbls if HasLevLbls else Levels+1)
 
 			
 		plt.show()
 				
 				
 	except Exception as e:
-		Framework().messagebox(str(e), "Scatter Error!")
+		Framework().messagebox(str(e), "Column Error!")
