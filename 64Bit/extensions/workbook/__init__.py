f785d49a9d0427212f305e069042ad58ae988c49
Gökhan Bingöl <gbingol@hotmail.com>
2024-09-04 16:50:50 +0200

    work on worksheet's one

--- 64Bit/extensions/workbook/__init__.py
+++ 64Bit/extensions/workbook/__init__.py
@@ -1,16 +1,90 @@
 import pathlib
 
 import _sci.extension as ext
-from _sci.framework import Framework
+from _sci import Framework, Workbook, RightClickEvent
 
 
 def runfile(x):
 	Framework().RunPyFile(x)
 
 
-if __name__ == "__main__":
-	CurFile = str(__file__)
-	CurFolder = pathlib.Path(__file__).parent
+
+def Save(param):
+	import numpy as np
+	import wx
+
+	try:
+		ws = Workbook().activeworksheet()
+		rng = ws.selection()
+		if rng.ncols()<2:
+			raise RuntimeError("At least 2 columns of data required")
+		
+		MainLst = rng.tolist(axis = 1)
+
+		Framework.Enable(False)
+		
+		wildcard = "Text files (*.txt)|*.txt"  if param =="txt" else "CSV files (*.csv)|*.csv"
+		dlg = wx.FileDialog(None, style=wx.FD_SAVE, wildcard= wildcard)
+		dlg.ShowModal()
+
+		Framework.Enable(True)
+		
+		path = str(dlg.GetPath())
+		if path =="":
+			return
+		
+		arr = np.array(MainLst)
+		arr[arr == None] = ""
+		
+		np.savetxt(path, arr, delimiter="\t" if param == "txt" else ",", fmt="%s")
+	except Exception as e:
+		Framework().messagebox(str(e),  "Export Error")
+
+
+
+def _rightclick(file):
+	CurFile = str(file)
+	CurFolder = pathlib.Path(file).parent
+
+	btnCreateVar = ext.Button("Create Variable...", 
+					  CurFolder/"icons/py_logo32.png", 
+					  CurFile, 
+					  runfile, CurFolder / "frmCreateVar.py")
+
+	btnImportVar = ext.Button("Process and/or Import...", 
+					CurFolder/"icons/py_logo32.png", 
+					CurFile, 
+					runfile, CurFolder / "processimport.py")
+
+
+	Workbook().AppendMenuItem() #menu separator
+	Workbook().AppendMenuItem(btnCreateVar)
+	Workbook().AppendMenuItem(btnImportVar)
+
+
+	btnExport_Txt = ext.Button("Text File", 
+					"", 
+					CurFile, 
+					Save, "txt")
+
+	btnExport_CSV = ext.Button("CSV File", 
+					"", 
+					CurFile, 
+					Save, "csv")
+	
+
+	menuExport = ext.Menu("Export", CurFolder / "icons/export.png")
+
+	menuExport.add(btnExport_CSV)
+	menuExport.add(btnExport_Txt)
+
+	Workbook().AppendMenuItem(menuExport)
+
+
+
+def setuppage(file):
+	CurFile = str(file)
+	CurFolder = pathlib.Path(file).parent
 
 	btnSort = ext.Button("Sort", 
 					  CurFolder/"icons/sort.jpg", 
@@ -40,3 +114,9 @@ if __name__ == "__main__":
 	page.add(btnExtensionMngr)
 
 	Framework().ToolBar_AddPage(page)
+
+
+
+if __name__ == "__main__":
+	setuppage(__file__)
+	Workbook().bind(RightClickEvent(), _rightclick, __file__)
